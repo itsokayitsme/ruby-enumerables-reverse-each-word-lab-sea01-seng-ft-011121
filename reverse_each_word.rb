32e14a8b4338e1d1915116860d82dd7784db5dd9
Johnny Cao <jcao0928@gmail.com>
2021-01-05 00:47:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,4 @@ end
 def reverse_each_word(reverseEach)
     reverseEach.split.collect {|word| word.reverse}.join(" ")
 end
-reverse_each_word("Hello")
+reverse_each_word("Hello there, and how are you?")
